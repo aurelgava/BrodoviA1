6477c3a1e14b540065fc0d5192f788898aec2295
Aurel Gava <10219aleksazivanov@etsntesla.edu.rs>
2022-10-18 11:00:33 +0200

    trenutni broj

--- src/more/Brod.java
+++ src/more/Brod.java
@@ -5,10 +5,16 @@ public class Brod {
     int kapacitet;
     Mornar[] mornars;
 
+    int trenutnibr;
+
     public Brod(String ime,int kapacitet, Mornar mornar) {
         mornars=new Mornar[kapacitet];
         this.ime=ime;
         this.kapacitet=kapacitet;
         this.mornars[0]=mornar;
+        trenutnibr=1;
+    }
+    public void dodajMorn(Mornar m){
+        if()
     }
 }
